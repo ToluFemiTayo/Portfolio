f9a487235f8caab00faacf09dd09d9bb28823647
ToluFemiTayo <omodarafemi2123@gmail.com>
2018-11-01 18:16:13 +0100

    modification

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
-$(".button").on("click", function (e) {
-    console.log(e);
-    $(".sidebar").toggleClass("show");
-  });
+// $(".button").on("click", function (e) {
+//     console.log(e);
+//     $(".sidebar").toggleClass("show");
+// });
+
+
+window.onscroll = function() {myFunction()};
+    
+var header = document.getElementById("header__wrap");
+var sticky = header.offsetTop;
   
+function myFunction() {
+ if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
