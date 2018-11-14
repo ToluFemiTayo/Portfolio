c3287e6fe38a4583e13d9afd0bdd1702b60444ca
ToluFemiTayo <omodarafemi2123@gmail.com>
2018-11-14 15:31:33 +0100

    modification

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,49 +14,49 @@ $(".btn").on("click", function (e) {
 });
 
 // form fill
-var _form = document.getElementsByClassName("form__fill")[0];
-var _inputs = Array.from(_form.getElementsByClassName("form__contol"));
-var _error = 0;
-var keyupHandler = function (e) {
-  var _next = e.target.nextElementSibling;
-  if (e.target.name === "name" ) {
-   if (e.target.value.length < 10) {
-    this.classList.add("is__invalid");
-    _next.classList.remove("d__none");
-   }
-   else {
-    this.classList.remove("is__invalid");
-    _next.classList.add("d__none");
-   }
-  }
-  else if (e.target.name === "email") {
-   var regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-   var value = e.target.value;
-   if (!regExp.test(String(value).toLowerCase())) {
-    this.classList.add("is__invalid");
-    _next.classList.remove("d__none");
-   }
-   else {
-    this.classList.remove("is__invalid");
-    _next.classList.add("d__none");
-   }
-  }
-  else if (e.target.name === "message") {
-   if (e.target.value.length < 20) {
-    this.classList.add("is__invalid");
-    _next.classList.remove("d__none");
-   }
-   else {
-    this.classList.remove("is__invalid");
-    _next.classList.add("d__none");   
-   };
-  }  
-};
-_inputs.forEach(function (eachInputs) {
-    eachInputs.addEventListener("keyup", keyupHandler);
-  });
- _form.addEventListener("submit")
-e.preventDefault();
+// var _form = document.getElementsByClassName("form__fill")[0];
+// var _inputs = Array.from(_form.getElementsByClassName("form__contol"));
+// var _error = 0;
+// var keyupHandler = function (e) {
+//   var _next = e.target.nextElementSibling;
+//   if (e.target.name === "name" ) {
+//    if (e.target.value.length < 10) {
+//     this.classList.add("is__invalid");
+//     _next.classList.remove("d__none");
+//    }
+//    else {
+//     this.classList.remove("is__invalid");
+//     _next.classList.add("d__none");
+//    }
+//   }
+//   else if (e.target.name === "email") {
+//    var regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//    var value = e.target.value;
+//    if (!regExp.test(String(value).toLowerCase())) {
+//     this.classList.add("is__invalid");
+//     _next.classList.remove("d__none");
+//    }
+//    else {
+//     this.classList.remove("is__invalid");
+//     _next.classList.add("d__none");
+//    }
+//   }
+//   else if (e.target.name === "message") {
+//    if (e.target.value.length < 20) {
+//     this.classList.add("is__invalid");
+//     _next.classList.remove("d__none");
+//    }
+//    else {
+//     this.classList.remove("is__invalid");
+//     _next.classList.add("d__none");   
+//    };
+//   }  
+// };
+// _inputs.forEach(function (eachInputs) {
+//     eachInputs.addEventListener("keyup", keyupHandler);
+//   });
+//  _form.addEventListener("submit")
+// e.preventDefault();
 
 // Js on form
 
